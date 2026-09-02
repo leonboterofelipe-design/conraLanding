a5e4d845a4fb3a04de830c2a4a4056b33db89a73
leonboterofelipe <leonboterofelipe@gmail.com>
2026-09-02 16:46:51 -0500

    a

--- conra-fba-website/app/page.js
+++ conra-fba-website/app/page.js
@@ -5,7 +5,7 @@ import AboutConrado from '@/components/sections/AboutConrado';
 import Testimonials from '@/components/sections/Testimonials';
 import FAQ from '@/components/sections/FAQ';
 import CTA from '@/components/sections/CTA';
-import QuienSoyPage from './conrafba/quien-soy/page';
+import QuienSoyPage from './conraFBA/quien-soy/page';
 
 export default function Page() {
   return (
