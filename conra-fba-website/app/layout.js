fe639ab43f34df54c535ad59fe6d8a9a185710e7
leonboterofelipe <leonboterofelipe@gmail.com>
2026-09-17 16:13:10 -0500

    v10Favicon

--- conra-fba-website/app/layout.js
+++ conra-fba-website/app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
     siteName: 'Conra FBA',
     images: [
       {
-        url: '/favicon.png',
+        url: 'favicon.png',
         width: 1200,
         height: 630,
       },
