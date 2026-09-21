f20cd40670ea25cc2461a23db5889b718807da6f
leonboterofelipe <leonboterofelipe@gmail.com>
2026-09-20 20:09:08 -0500

    v16img

--- conra-fba-website/app/conra-fba/quien-soy/page.js
+++ conra-fba-website/app/conra-fba/quien-soy/page.js
@@ -12,7 +12,7 @@ export default function QuienSoyPage() {
         <div className="flex justify-center">
           <div className="relative w-56 h-56 md:w-64 md:h-64 rounded-full overflow-hidden border-2 border-brand-500 shadow-2xl">
             <Image
-              src="/assets/images/DSC01098.JPG"
+              src="/assets/DSC01098.JPG"
               alt="Conrado Loaldi"
               fill
               className="object-cover"
